7ed4e6fab256c55fa3b8d9f5dc21a504c12fc5dc
thiseaskambas <tkambas@yahoo.com>
2022-02-14 10:04:45 -0400

    base functionality

--- src/generator.js
+++ src/generator.js
@@ -1,7 +1,32 @@
-function createProject(name = 'New Project', setDefault = false) {
+function createTodoList(setTitle) {
+   const title = setTitle;
+   const projects = [];
+   const createNewProject = function (title, isDefault) {
+      const newProject = createProject(title, isDefault);
+      projects.push(newProject)
+   }
+   return { title, projects, createNewProject }
+}
+
+function createProject(setTitle = 'New Project', setDefault = false) {
    const todos = [];
    const isDefault = setDefault;
-   const title = name;
-
-   return { todos, isDefault, title }
+   const title = setTitle;
+   const createNewTodo = function (setTitle, setDate) {
+      const newTodo = createTodo(setTitle, setDate)
+      todos.push(newTodo)
+   }
+   return { todos, isDefault, title, createNewTodo }
 }
+
+function createTodo(setTitle = "Unnamed to-do", date = undefined) {
+   const title = setTitle;
+   const description = null;
+   const dueDate = date;
+   const priority = 0;
+   const complete = false;
+
+   return { title, description, dueDate, priority, complete }
+}
+
+export { createProject, createTodo, createTodoList }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('hello')
+import { createProject, createTodo, createTodoList } from "./generator"
+
+
+const myList = createTodoList('MYlistTEST')
+myList.createNewProject('NewProtz', true)
+myList.projects[0].createNewTodo("TODOsoooon", "Feb 22")
+console.log(myList.projects[0].todos[0])
